c6d1dcf939c961bd2a618d853a376fe5c0c5952d
Claudio Karklis <claudiokarklis@gmail.com>
2023-03-13 18:24:55 -0300

    i18n + react-i18next + i18next-browser-languagedetector

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'Inglês'},
+  pt: { nativeName: 'Português'}
+};
+
+function App() {
+  const { t, i18n } = useTranslation();
+  const [count, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
+              i18n.changeLanguage(lng);
+              setCounter(count + 1);
+            }}>
+              {lngs[lng as keyof typeof lngs].nativeName}
+            </button>
+          ))}
+        </div>
+        <p>
+          <i>{t('counter', { count })}</i>
+        </p>
+        <p>
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('description.part2')}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
